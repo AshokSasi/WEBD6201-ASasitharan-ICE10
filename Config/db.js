36f834054fcfbdd600458b680b4e0908ca0243d2
Ashok Sasitharan <asasitharan28@gmail.com>
2021-03-31 11:54:16 -0400

    added mongoose

---
+++ Config/db.js
@@ -0,0 +1,11 @@
+"use strict";
+let mongoDBPath = "mongodb://localhost/webd6201";
+let sessionSecret = "someSecret";
+function myFunction() {
+    console.log("This does nothing");
+}
+module.exports = {
+    Path: mongoDBPath,
+    Secret: sessionSecret
+};
+//# sourceMappingURL=db.js.map
